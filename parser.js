6696976e9dff6f04349c19699c141a09b91baddc
Aymeric Bouchet <aymeric.bouchet@viacesi.fr>
2022-01-11 14:19:43 +0100

    🎉✅📦️Add test framework Must and Mocha

---
+++ parser.js
@@ -0,0 +1,10 @@
+// let sudokuString = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+
+// let lines = [];
+
+// lines = sudokuString.split('')
+
+
+// console.log(lines)
+
+// console.log(sudokuString.slice(0,9))
